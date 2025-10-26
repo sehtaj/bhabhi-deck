ab7a856e5607f373a0cdeb0ab10e2ad2b6a08c87
Sehtaj Singh <batthsehtaj10@gmail.com>
2025-10-26 18:32:12 +0530

    fix: removed backdrop blur from navbar

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -7,7 +7,7 @@ import { Typography } from "@/components/ui/typography";
 
 export function NavBar() {
   return (
-    <nav className="px-6 py-4 flex justify-between items-center fixed top-0 left-0 right-0 z-50 bg-transparent backdrop-blur-md">
+    <nav className="px-6 py-4 flex justify-between items-center fixed top-0 left-0 right-0 z-50 bg-transparent">
       {/* Logo */}
       <Link href="/" className="flex items-center gap-2">
         <Image
